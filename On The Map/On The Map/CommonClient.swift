e86d92cbc5afb36931c8921974450cee74d84ac2
vishnupillai9 <vishnu.pillai@me.com>
2015-04-02 17:16:50 +0530

    Committed by Udacity

---
+++ On The Map/On The Map/CommonClient.swift
@@ -0,0 +1,42 @@
+//
+//  CommonClient.swift
+//  On The Map
+//
+//  Created by Vishnu on 01/04/15.
+//  Copyright (c) 2015 Vishnu Pillai. All rights reserved.
+//
+
+import Foundation
+
+class CommonClient {
+    
+    //Helper function for parsing error data
+    class func errorForData(data: NSData?, response: NSURLResponse?, error: NSError) -> NSError {
+        
+        if let parsedResult = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil) as? [String : AnyObject] {
+            
+            if let errorMessage = parsedResult["error"] as? String {
+                let userInfo = [NSLocalizedDescriptionKey : errorMessage]
+                return NSError(domain: "Error", code: 1, userInfo: userInfo)
+            }
+            
+        }
+        
+        return error
+    }
+    
+    //Helper function for parsing JSON data
+    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsingError: NSError? = nil
+        let parsedResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+        
+        if let error = parsingError? {
+            completionHandler(result: nil, error: error)
+        } else {
+            completionHandler(result: parsedResult, error: nil)
+        }
+        
+    }
+    
+}
